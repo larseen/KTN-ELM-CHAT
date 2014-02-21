6e25604102fd905e3fbfdf1419266791df83dd9b
shorm <olavmarkussen@hotmail.no>
2014-02-21 03:28:34 +0100

    Doing minor stuff on Client

--- src/Client.java
+++ src/Client.java
@@ -1,22 +1,29 @@
+import java.net.InetAddress;
 import java.util.Scanner;
-
 public class Client implements Runnable {
 	
+	private String unserName;
+	
+	private Thread mainThread = new Thread();
 	private ServerHandler serverHandler;
+	
+	//InetAddress address= new InetAddress();
+	private int port;
 	
 	@Override
 	public void run(){
 		while(true){
+			@SuppressWarnings("resource")
 			String input = new Scanner(System.in).nextLine();
-			handleInput(input);
+			resolveInput(input);
 		}
 	}
 	
-	private void handleInput(String input){
+	private void resolveInput(String input){
 		
 	}
 	
-	private void pushMessage(){
+	private void pushMessage(String message){
 		
 	}
 	
