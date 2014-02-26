e6fa179f1bace450a46bc77a32e490833f01c5dd
mabergen <mari_bergendahl@hotmail.com>
2014-02-26 11:38:19 +0100

    Added isUsernameAvailable

--- src/chat/Server.java
+++ src/chat/Server.java
@@ -1,11 +1,44 @@
 package chat;
 
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Server {
 
+	private Socket serverSocket;
+	private InetAddress address;
+	private Integer port;
+	private ArrayList<ClientHandler> clients;
+	
 	public void getMessages() {
-		// TODO Auto-generated method stub
 		
 	}
+	
+	
+	
+	
+	public void pushMessages(String message){
+		
+	}
+	
+	
+	
+	
+	public boolean isUsernameAvailable(String username){
+		for(ClientHandler client : clients){
+			if(username.equals(client.getUsername())){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 	
